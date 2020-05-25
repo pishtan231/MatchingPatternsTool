2da93d131ce8d8f9e113bd0f4f0dec2d875197c7
l.ossi@avatrade.com <l.ossi@avatrade.com>
2020-05-25 05:29:46 +0300

    Finding patterns in binary files - 1. Find/Replace regex-like patterns. 2. Matching bytes, by a given pattern. 3. Finding offsets of patterns in a binary file. 4. Produce JSON with the required results.

--- README.md
+++ README.md
@@ -1,1 +1,201 @@
 # MatchingPatternsTool
+
+Main functionality -
+1. Find/Replace regex-like patterns.
+2. Matching bytes, by a given pattern.
+3. Finding offsets of patterns in a binary file.
+
+For each functionality it produced
+JSON with the required results.
+
+Generic run:
+main.py -p <PATTERN> if <FILENAME>
+
+
+EXAMPLES
+Search for the hex bytes "C54C6BA5FEE1", "CDD0F08AE8" in the file some_file.bin:
+
+
+python bin24.py -g {\"A54C6BA5FEE1\":\"value1\"\,\"BDD0F08AE8\":\"value3\} -f some_file.bin -r
+
+Will replace data in file and will also produce produce JSON, e.g:
+[
+    {
+        "Pattern": "A54C6BA5FEE1",
+        "Start_offset": {
+            "Decimal": "2307695617",
+            "Hexadecimal": "898CA401"
+        },
+        "End_offset": {
+            "Decimal": "2307695623",
+            "Hexadecimal": "898CA407"
+        }
+    },
+    {
+        "Pattern": "BDD0F08AE8",
+        "Start_offset": {
+            "Decimal": "6161538689",
+            "Hexadecimal": "16F419E81"
+        },
+        "End_offset": {
+            "Decimal": "6161538694",
+            "Hexadecimal": "16F419E86"
+        }
+    }
+]
+
+You can Input patterns as a list
+python bin24.py -z "FF64FF" "A4A5A6" "AE" -f my_file.bin
+
+Will produce JSON, e.g:
+
+{
+    "repeating_bytes": [
+        {
+            "range": [
+                0,
+                2
+            ],
+            "size": 2,
+            "repeating_byte": "A4A5A6"
+        },
+        {
+            "range": [
+                8,
+                10
+            ],
+            "size": 2,
+            "repeating_byte": "AE"
+        },
+        {
+            "range": [
+                14,
+                18
+            ],
+            "size": 4,
+            "repeating_byte": "AEAE"
+        },
+        {
+            "range": [
+                23,
+                25
+            ],
+            "size": 2,
+            "repeating_byte": "AE"
+        },
+        {
+            "range": [
+                29,
+                31
+            ],
+            "size": 2,
+            "repeating_byte": "AE"
+        }
+    ]
+}
+
+
+python bin24.py -g {\"C54C6BA5FEE1\":\"value1\"\,\"CDD0F08AE8\":\"value3\"} -f some_file.bin 
+
+Will produce JSON, e.g:
+[
+    {
+        "Pattern": "C54C6BA5FEE1",
+        "Start_offset": {
+            "Decimal": "2307695617",
+            "Hexadecimal": "898CA401"
+        },
+        "End_offset": {
+            "Decimal": "2307695623",
+            "Hexadecimal": "898CA407"
+        }
+    },
+    {
+        "Pattern": "CDD0F08AE8",
+        "Start_offset": {
+            "Decimal": "6161538689",
+            "Hexadecimal": "16F419E81"
+        },
+        "End_offset": {
+            "Decimal": "6161538694",
+            "Hexadecimal": "16F419E86"
+        }
+    }
+]
+
+
+python main.py -z "/xaf" -f some_file.bin
+
+will produce json with all the repeating bytes 
+
+Will produce JSON, e.g:
+
+{
+    "repeating_bytes": [
+        {
+            "range": [
+                0,
+                2
+            ],
+            "size": 2,
+            "repeating_byte": "Af"
+        },
+        {
+            "range": [
+                8,
+                10
+            ],
+            "size": 2,
+            "repeating_byte": "af"
+        },
+        {
+            "range": [
+                14,
+                18
+            ],
+            "size": 4,
+            "repeating_byte": "af"
+        },
+        {
+            "range": [
+                23,
+                25
+            ],
+            "size": 2,
+            "repeating_byte": "af"
+        },
+        {
+            "range": [
+                29,
+                31
+            ],
+            "size": 2,
+            "repeating_byte": "af"
+        }
+    ]
+}
+
+
+Searches for the hexidecimal pattern "PA##TT##ERN" in some_file will produce 
+JSON with values where ##=ANy valid char=.*
+python main.py -p "PA##TT##ERN" some_file.bin
+
+[
+"PATTERN",
+PAA3TTA4EN",
+...
+]
+
+args optionS
+
+Optional Arguments:
+  -f FILE, --file FILE  file to read search pattern from
+  -p PATTERN, --pattern PATTERN
+                        a hexidecimal pattern to search for
+ 
+  -r replace, --replace replace bin/hex with map vals
+                        
+  -l FILE, --log FILE   write matched offsets to FILE, instead of standard
+                        output
+  -z, --zlist          list of pattern to search
+                    
---
+++ main.py
@@ -0,0 +1,215 @@
+import re
+import multiprocessing as mp
+import json
+from argparse import ArgumentParser
+from sys import stdout
+import os
+import uuid
+
+STDOUT = stdout
+
+DESCRIPTION = ''  # noqa
+__FILE_NAME_SUFFIX__ = '.json'
+DIR_NAME = 'output_files'
+
+
+def get_args():
+    p = ArgumentParser(description=DESCRIPTION)
+
+    def add(s, **kwargs):
+        args = s.split(':')
+
+        value = args.pop(2)
+        if value: kwargs['type'] = eval(value)
+        value = args.pop(2)
+        if value: kwargs['metavar'] = value
+        value = args.pop(2)
+        if value: kwargs['dest'] = value
+
+        p.add_argument(*args, **kwargs)
+
+    p.add_argument('-z', '--patterns_list', nargs='+', help='<Required> Set flag', required=False, default=[])
+    p.add_argument('-g', '--patterns_map', type=json.loads)
+    p.add_argument('-r', '--replace', type=bool, default=False)
+    p.add_argument('-f', '--file', type=str,
+                   metavar='FILE', dest='file_read',
+                   help='file to read search pattern from')
+    p.add_argument('-p', '--pattern', type=str,
+                   metavar='PATTERN', dest='ppattern',
+                   help='a hexidecimal pattern to search for')
+    p.add_argument(type=str,
+                   metavar='FILE', dest='fsearch', nargs='*',
+                   help='files to search within')
+    return p.parse_args()
+
+
+class HandlePatterns():
+    def __init__(self, args):
+        self.matched_patterns = []
+
+        if args.file_read:
+            self.file_name = args.file_read
+
+        if args.patterns_map:
+            self.patterns_from_user = args.patterns_map
+            self.processed_patterns = self.parallel_runs(
+                args.patterns_map.keys()
+            )
+            self._debug_search()
+            if args.replace:
+                self.replace_patterns()
+
+        if args.patterns_list:
+            repeat_pattern = args.patterns_list[0]
+            if repeat_pattern[:2] == "/x":
+                repeat_pattern = repeat_pattern[2:]
+            self.search_byte(repeat_pattern)
+
+    @classmethod
+    def input_to_pattern(cls, pattern):
+        initial_input = pattern
+        pattern = [p for p in pattern.split("##")]
+        pattern = [bytes.fromhex(p) for p in pattern]
+        len_pattern = len(b"?".join(pattern))
+        pattern = [re.escape(p) for p in pattern]
+        pattern = b".".join(pattern)
+        regex = re.compile(pattern, re.DOTALL + re.MULTILINE)
+        return {
+            'len_pattern': len_pattern,
+            'regex': regex,
+            'pattern_from_user': initial_input,
+        }
+
+    def replace_patterns(self):
+        for key, val in self.patterns_from_user.items():
+            file = open(self.file_name, 'rb')
+            save_to_replace = file.read()
+            file.close()
+            to_replace = bytearray.fromhex(key)
+            new = bytearray.fromhex(val)
+            save_to_replace = save_to_replace.replace(to_replace, new)
+            file_open_replace = open(self.file_name, 'wb')
+            file_open_replace.write(save_to_replace)
+            file_open_replace.close()
+
+    @classmethod
+    def search_match_in_row(cls, pattern, match_start, offset, match_end):
+        return {
+            "Pattern": ("%s" % pattern),
+            "Start_offset": {
+                "Decimal": ("%d" % (offset + match_start)),
+                "Hexadecimal": ("%X" % (offset + match_start)),
+            },
+            "End_offset": {
+                "Decimal": ("%d" % (offset + match_end)),
+                "Hexadecimal": ("%X" % (offset + match_end)),
+            }
+        }
+
+    @staticmethod
+    def check_match_parallel(self, reg_expression, buffer):
+        return reg_expression.search(buffer)
+
+    def parallel_runs(self, data_list):
+        pool = mp.Pool(processes=len(data_list))
+        result = pool.map(self.input_to_pattern, data_list)
+        return result
+
+    def is_match(self, buffer, offset, min_matched):
+        all_matched = []
+        for reg in self.processed_patterns:
+            match = reg['regex'].search(buffer)
+            if match:
+                min_matched = match.start() if min_matched == -1 else max(min_matched, match.start())
+                all_matched.append(match)
+                self.matched_patterns.append(
+                    self.search_match_in_row(reg['pattern_from_user'], match.start(), offset, match.end())
+                )
+        return min_matched
+
+    def _debug_search(self, new_val=0):
+        max_len = 0
+        all_patterned = []
+        for new_pattern in self.processed_patterns:
+            max_len = max(max_len, new_pattern['len_pattern'])
+            all_patterned.append(new_pattern)
+        pats_len = len(self.processed_patterns)
+
+        file_size = os.path.getsize(self.file_name)
+        max_size = 1024 if file_size > pow(1024, 2) else 8
+
+        try:
+            min_matched = -1
+            with open(self.file_name, 'rb') as file:
+                buffer = file.read(max_size + max_len)
+                offset = 0
+                all_matched = []
+                min_matched = self.is_match(buffer, offset, min_matched)
+                while True:
+                    if not len(all_matched) == pats_len:
+                        offset += max_size
+                        buffer = buffer[max_size:]
+                        buffer += file.read(max_size)
+                        all_matched = []
+                        min_matched = self.is_match(buffer, offset, min_matched)
+                    else:
+                        all_matched = []
+                        min_matched = self.is_match(buffer, offset, min_matched)
+                    if len(buffer) <= max_len:
+                        if not os.path.exists(DIR_NAME):
+                            os.makedirs(DIR_NAME)
+
+                        base_filename = uuid.uuid4()
+                        file_name_unique = os.path.join(DIR_NAME, str(base_filename) + __FILE_NAME_SUFFIX__)
+                        with open(file_name_unique, 'w', encoding='utf-8') as file_to_write:
+                            json.dump(self.matched_patterns, file_to_write, ensure_ascii=False, indent=4)
+                        break
+
+        except Exception as e:
+            print('error\n')
+            print(e)
+
+
+    def readInChunks(self, fileObj, chunkSize=1024):
+        while True:
+            data = fileObj.read(chunkSize)
+            if not data:
+                break
+            yield data
+
+    def search_byte(self, byte_to_find):
+        base_filename = uuid.uuid4()
+        file_name_unique = os.path.join(DIR_NAME, str(base_filename) + __FILE_NAME_SUFFIX__)
+
+        format_byte = "({})".format(byte_to_find) + "\\1*"
+        regex = re.compile(format_byte)
+
+        data = {'repeating_bytes': []}
+        iter_start_point = 0
+
+        file_to_read = open(self.file_name, 'rb')
+        for chuck in self.readInChunks(file_to_read):
+            iter_chunk = regex.finditer(chuck.hex())
+            if iter_chunk:
+                for it in iter_chunk:
+                    start_point = (int(it.start() / 2) + (32 * iter_start_point))
+                    grouped_pattern = it.group()
+                    data['repeating_bytes'].append(
+                        {
+                            'range': (start_point, start_point + len(grouped_pattern)),
+                            'size': len(grouped_pattern),
+                            'repeating_byte': grouped_pattern,
+                        }
+                    )
+            iter_start_point += 32
+        file_to_read.close()
+
+        if not os.path.exists(DIR_NAME):
+            os.makedirs(DIR_NAME)
+        with open(file_name_unique, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+
+
+if __name__ == "__main__":
+    args1 = get_args()
+    patterns_obj = HandlePatterns(args1)
